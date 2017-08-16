2e724a45734a2f9938c358ca31e9dc2fac581b4a
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2017-08-16 16:49:35 +0300

    fix: fixed uptimez

--- server/infra/uptimez.route.js
+++ server/infra/uptimez.route.js
@@ -12,7 +12,7 @@ export default class UserRoute extends BaseRoute {
   routes() {
     this.router.get('/', async (ctx, next) => {
       // Check that database is up
-      await ctx.getTx().any(`
+      await ctx.myappCtx.getTx().any(`
         SELECT *
         FROM example_user
         LIMIT 1
