72d84e7ca1b89f4922b64b94c8383349e3f5500c
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2017-08-16 15:51:51 +0300

    feat: uptimez route

---
+++ server/infra/uptimez.route.js
@@ -0,0 +1,30 @@
+import koaRouter from 'koa-router';
+
+import BaseRoute from '../common/base.route';
+
+export default class UserRoute extends BaseRoute {
+
+  constructor(router) {
+    super();
+    this.router = router || koaRouter();
+  }
+
+  routes() {
+    this.router.get('/', async (ctx, next) => {
+      // Check that database is up
+      await ctx.getTx().any(`
+        SELECT *
+        FROM example_user
+        LIMIT 1
+      `);
+      next();
+    });
+
+    return this.router.routes();
+  }
+
+  allowedMethods() {
+    return this.router.allowedMethods();
+  }
+
+}
--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,10 @@
+import UptimezRoute from './infra/uptimez.route';
 import FileRoute from './examples/files/file.route';
 import UserRoute from './examples/users/user.route';
 
 const routes = (router) => {
+  const uptimezRoute = new UptimezRoute();
+  router.use('/uptimez', uptimezRoute.routes(), uptimezRoute.allowedMethods());
   const fileRoute = new FileRoute();
   router.use('/files', fileRoute.routes(), fileRoute.allowedMethods());
   const userRoute = new UserRoute();
