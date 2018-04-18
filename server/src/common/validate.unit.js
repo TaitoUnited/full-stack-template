640a869c18db18d8002205c14e4b1def65c2d6c4
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2018-04-18 11:59:01 +0300

    chore: Added dummy unit test

---
+++ server/src/common/validate.unit.js
@@ -0,0 +1,9 @@
+import chai from 'chai'; // eslint-disable-line
+
+const { expect } = chai;
+
+describe('dummy test', () => {
+  it('does nothing', () => {
+    expect(true).to.equal(true);
+  });
+});
