e7a1a51b541a467bb62e20fd544e067f2adc1088
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-02-19 14:40:15 +0200

    chore: run cypress on on local env

--- admin/test.sh
+++ admin/test.sh
@@ -3,11 +3,11 @@
 export suite_name="${1:-*}"
 export test_name="${2:-*}"
 
-if [[ "${suite_name}" == "cypress"* ]] && [[ "${taito_mode:-}" != "ci" ]]; then
+if [[ "${suite_name}" == "cypress"* ]] && [[ "${taito_env:?}" == "local" ]]; then
   npm run cypress:run
-elif [[ "${suite_name}" == "cypress"* ]] && [[ "${taito_mode}" == "ci" ]]; then
-  echo "SKIP: Cypress tests are currently disabled in ci mode because of a"
-  echo "connection problem: https://github.com/cypress-io/cypress/issues/1639"
+elif [[ "${suite_name}" == "cypress"* ]] && [[ "${taito_env:?}" != "local" ]]; then
+  echo "SKIP: Cypress tests are currently disabled for non-local env because of"
+  echo "a connection problem: https://github.com/cypress-io/cypress/issues/1639"
 else
   # TODO pass suite name and test name
   npm run test
--- client/test.sh
+++ client/test.sh
@@ -3,11 +3,11 @@
 export suite_name="${1:-*}"
 export test_name="${2:-*}"
 
-if [[ "${suite_name}" == "cypress"* ]] && [[ "${taito_mode:-}" != "ci" ]]; then
+if [[ "${suite_name}" == "cypress"* ]] && [[ "${taito_env:?}" == "local" ]]; then
   npm run cypress:run
-elif [[ "${suite_name}" == "cypress"* ]] && [[ "${taito_mode}" == "ci" ]]; then
-  echo "SKIP: Cypress tests are currently disabled in ci mode because of a"
-  echo "connection problem: https://github.com/cypress-io/cypress/issues/1639"
+elif [[ "${suite_name}" == "cypress"* ]] && [[ "${taito_env:?}" != "local" ]]; then
+  echo "SKIP: Cypress tests are currently disabled for non-local env because of"
+  echo "a connection problem: https://github.com/cypress-io/cypress/issues/1639"
 elif [[ "${suite_name}" == "suite1" ]]; then
   npm run test
 elif [[ "${suite_name}" == "suite2" ]]; then
