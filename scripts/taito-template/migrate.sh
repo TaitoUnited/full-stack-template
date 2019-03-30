6d612547e654fee9b19857fd6a4f2209f024299e
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-03-30 12:45:38 +0200

    feat: project migrate

--- scripts/taito-template/migrate.sh
+++ scripts/taito-template/migrate.sh
@@ -8,11 +8,20 @@
 # Move old files away from project root
 rm -rf "${template_project_path}/old_root" 2> /dev/null
 mkdir -p "${template_project_path}/old_root"
-mv !(.git) old_root
+shopt -s extglob dotglob
+mv !(old_root) old_root
+shopt -u extglob dotglob
+mv old_root/.git .
 
 # Copy all files from template root
 rm -rf .git
 yes | cp -- * "${template_project_path}" 2> /dev/null
 yes | cp -- .* "${template_project_path}" 2> /dev/null
 
+echo
+echo
+echo "--- Manual steps ---"
+echo
+echo "Your old files are located in 'old_root/'. Move files to correct locations"
+echo "using the new folder structure and make it work locally on Docker Compose first."
 echo
