e90c7eabc76ac9e284f58ea639dd84d4a75911be
Temzasse <teemu.taskula@taitounited.fi>
2019-04-03 16:26:12 +0300

    chore: add utils

---
+++ client/src/common/constants.ts
@@ -0,0 +1,6 @@
+export const BREAKPOINTS = {
+  sm: 700,
+  // NOTE: `md` is not really needed since tablet size is between `sm` and `lg`
+  // md: 900,
+  lg: 1025,
+};
--- client/src/common/utils/fn.utils.ts
+++ client/src/common/utils/fn.utils.ts
@@ -8,3 +8,31 @@ export const times = (timesArg: number) => {
       });
   };
 };
+
+export const scrollToBottom = () => {
+  window.scrollTo(0, document.body.scrollHeight);
+};
+
+export const range = (n: number) => [...Array(n).keys()];
+
+export const sleep = (ms: number) =>
+  new Promise(resolve => setTimeout(resolve, ms));
+
+export const clamp = (value: number, min: number, max: number) =>
+  Math.max(min, Math.min(max, value));
+
+export const randomNumBetween = (min: number, max: number) =>
+  Math.floor(Math.random() * max) + min;
+
+export const isNumeric = (n: any) => !isNaN(parseFloat(n)) && isFinite(n);
+
+export const truncate = (str: string, len: number) => {
+  if (str.length > len) return `${str.substring(0, len - 3)}...`;
+  return str;
+};
+
+// eslint-disable-next-line
+export const noop = () => {};
+
+// eslint-disable-next-line
+export const noopWithArgs = (...args: any[]) => {};
