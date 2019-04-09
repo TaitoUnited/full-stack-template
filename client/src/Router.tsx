a0730dc2d1e21302da3c565879f00f42342ad30c
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-04-09 15:11:15 +0300

    fix: fix build

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -3,34 +3,16 @@ import { Route } from 'react-router-dom';
 import { ErrorBoundary } from '~common/ui';
 
 // Code split routes
-const Orders = React.lazy(() => import('./orders'));
 const Posts = React.lazy(() => import('./posts'));
-const Reports = React.lazy(() => import('./reports'));
 const Settings = React.lazy(() => import('./settings'));
 
 const Router = () => (
   <React.Suspense fallback="Loading...">
     <Route
-      path="/orders"
-      render={(navProps: any) => (
-        <ErrorBoundary>
-          <Orders {...navProps} />
-        </ErrorBoundary>
-      )}
-    />
-    <Route
       path="/posts"
       render={(navProps: any) => (
         <ErrorBoundary>
           <Posts {...navProps} />
-        </ErrorBoundary>
-      )}
-    />
-    <Route
-      path="/reports"
-      render={(navProps: any) => (
-        <ErrorBoundary>
-          <Reports {...navProps} />
         </ErrorBoundary>
       )}
     />
