a18aaf7e693b099a5c78db85a256c7ee1a1202a0
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-04-28 21:40:56 +0300

    fix: project create init

--- scripts/taito-template/init.sh
+++ scripts/taito-template/init.sh
@@ -252,7 +252,7 @@ sed -i "s/\${template_default_environments:?}/${template_default_environments}/g
 sed -i "s/\${template_default_organization:?}/${template_default_organization}/g" taito-config.sh
 sed -i "s/\${template_default_organization_abbr:?}/${template_default_organization_abbr}/g" taito-config.sh
 sed -i "s/\${template_default_git_organization:?}/${template_default_git_organization}/g" taito-config.sh
-sed -i "s/\${template_default_git_url:?}/${template_default_git_url//\//\\/}/g" taito-config.sh
+sed -i "s|\${template_default_git_url:?}|${template_default_git_url}|g" taito-config.sh
 sed -i "s/\${template_default_sentry_organization:?}/${template_default_sentry_organization}/g" taito-config.sh
 sed -i "s/\${template_default_domain:?}/${template_default_domain}/g" taito-config.sh
 sed -i "s/\${template_default_domain_prod:?}/${template_default_domain_prod}/g" taito-config.sh
@@ -265,8 +265,8 @@ sed -i "s/\${template_default_provider_region:?}/${template_default_provider_reg
 sed -i "s/\${template_default_provider_zone:?}/${template_default_provider_zone}/g" taito-config.sh
 sed -i "s/\${template_default_provider_region_prod:?}/${template_default_provider_region_prod}/g" taito-config.sh
 sed -i "s/\${template_default_provider_zone_prod:?}/${template_default_provider_zone_prod}/g" taito-config.sh
-sed -i "s/\${template_default_monitoring_uptime_channels_prod:-}/${template_default_monitoring_uptime_channels_prod//\//\\\/}/g" taito-config.sh
-sed -i "s/\${template_default_container_registry:?}/${template_default_container_registry}/g" taito-config.sh
+sed -i "s|\${template_default_monitoring_uptime_channels_prod:-}|${template_default_monitoring_uptime_channels_prod}|g" taito-config.sh
+sed -i "s|\${template_default_container_registry:?}|${template_default_container_registry}|g" taito-config.sh
 sed -i "s/\${template_default_source_git:?}/${template_default_source_git}/g" taito-config.sh
 sed -i "s/\${template_default_dest_git:?}/${template_default_dest_git}/g" taito-config.sh
 
@@ -418,7 +418,7 @@ fi
 sed -i "s/\$template_default_taito_image_username/${template_default_taito_image_username:-}/g" "${ci_script}"
 sed -i "s/\$template_default_taito_image_password/${template_default_taito_image_password:-}/g" "${ci_script}"
 sed -i "s/\$template_default_taito_image_email/${template_default_taito_image_email:-}/g" "${ci_script}"
-sed -i "s/\$template_default_taito_image/${template_default_taito_image}/g" "${ci_script}"
+sed -i "s|\$template_default_taito_image|${template_default_taito_image}|g" "${ci_script}"
 
 ##############################
 # Initialize semantic-release
