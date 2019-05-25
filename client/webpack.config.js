875a9ebf79cbb25df4f3f26b5c52d9407bcbd0d5
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-05-25 20:36:36 +0300

    feat: windows volume mount performace tuning

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,7 @@ const PWA_ICON_DIR = ASSETS_DIR + '/icon.png';
 const PUBLIC_PORT = process.env.COMMON_PUBLIC_PORT;
 const DEV_PORT = 8080;
 const DEV_POLL =
-  process.env.HOST_UNAME == 'Darwin'
+  process.env.HOST_UNAME == 'Darwin' && !process.env.DC_COMMAND
     ? 2000
     : undefined;
 
