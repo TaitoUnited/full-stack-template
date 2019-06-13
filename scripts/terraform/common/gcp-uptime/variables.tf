84b04bc0cc80ae03740d5c2f8f8e95d369377eee
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-06-13 15:44:05 +0300

    fix: uptime variables mandatory

--- scripts/terraform/common/gcp-uptime/variables.tf
+++ scripts/terraform/common/gcp-uptime/variables.tf
@@ -6,17 +6,13 @@ variable "taito_uptime_namespace_id" {}
 /* Monitoring */
 variable "taito_uptime_targets" {
   type = "list"
-  default = []
 }
 variable "taito_uptime_paths" {
   type = "list"
-  default = []
 }
 variable "taito_uptime_timeouts" {
   type = "list"
-  default = []
 }
 variable "taito_uptime_channels" {
   type = "list"
-  default = []
 }
