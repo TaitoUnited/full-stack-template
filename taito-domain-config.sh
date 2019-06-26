c17b5f9584a46557912aeda8a4f077b8cd37b285
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-06-26 16:01:45 +0300

    chore: prefer mydomain.com

--- taito-domain-config.sh
+++ taito-domain-config.sh
@@ -5,11 +5,11 @@
 # Domain settings for production environment
 ##########################################################################
 
-# Production domain name (e.g. www.mydomain.com)
+# Production domain name (e.g. mydomain.com)
 taito_domain=
 taito_domain=$taito_project-$taito_target_env.${template_default_domain_prod:?} # TEMPLATE-REMOVE
 
-# Production alternate domain (e.g. mydomain.com) that redirects to main domain
+# Production alternate domain (e.g. www.mydomain.com) that redirects to main domain
 taito_altdomain=
 
 # Default domain name (copy value to taito_domain if custom name is not required)
