7ab8482687c8ab5e7eb891fd18f56aaae540c52b
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-07-07 16:32:29 +0300

    chore: taito-env-prod-config.sh formatting

--- taito-env-prod-config.sh
+++ taito-env-prod-config.sh
@@ -5,20 +5,24 @@
 # Production environment settings
 ##########################################################################
 
-# Production domain name (e.g. mydomain.com)
+# --- Settings ---
+
+# Disable basic auth by setting this to false
+taito_basic_auth_enabled=true
+
+# --- Domain ---
+
+# Domain name (e.g. mydomain.com)
 taito_domain=
 taito_domain=$taito_project-$taito_target_env.${template_default_domain_prod:?} # TEMPLATE-REMOVE
 
-# Production alternate domain (e.g. www.mydomain.com) that redirects to main domain
+# Alternate domain name (e.g. www.mydomain.com) that redirects to main domain
 taito_altdomain=
 
 # Default domain name (copy value to taito_domain if custom name is not required)
 taito_default_domain=$taito_project-$taito_target_env.${template_default_domain_prod:?}
 
-# Disable production environment basic auth by setting this to false
-taito_basic_auth_enabled=true
-
-# Overrides for canary environment
+# Canary environment domain name
 if [[ $taito_target_env == "canary" ]]; then
   taito_domain=$taito_project-$taito_target_env.${template_default_domain_prod:?}
   taito_default_domain=$taito_project-$taito_target_env.${template_default_domain_prod:?}
