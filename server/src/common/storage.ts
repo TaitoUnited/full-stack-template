2b9b8ad696347a3c1ac406dba2f2b9745c296c61
Kasper Koho <antipaste@gmail.com>
2019-08-13 16:41:28 +0300

    feat(server): check storage connection in uptimez

---
+++ server/src/common/storage.ts
@@ -0,0 +1,16 @@
+import AWS from "aws-sdk";
+import config from "./config";
+
+const storage = new AWS.S3({
+  signatureVersion: "v4",
+  s3ForcePathStyle: config.S3_FORCE_PATH_STYLE,
+  endpoint: config.S3_URL,
+  region: config.S3_REGION,
+  accessKeyId: config.S3_KEY_ID,
+  secretAccessKey: config.S3_KEY_SECRET,
+  params: {
+    Bucket: config.S3_BUCKET
+  }
+});
+
+export default storage;
