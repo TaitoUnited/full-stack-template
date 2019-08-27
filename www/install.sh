b0a1af11d9b7f6617fd21b717ad516db81a71887
Jani Mustonen <janijohannes@kapsi.fi>
2019-08-27 13:23:39 +0300

    fix: bump www's ruby to 2.6.3 (Fixes Jekyll)

--- www/install.sh
+++ www/install.sh
@@ -12,7 +12,19 @@ apt install /tmp/hugo_extended_${HUGO_VERSION}_Linux-64bit.deb
 rm -f /tmp/hugo_extended_${HUGO_VERSION}_Linux-64bit.deb
 
 # Install Jekyll. NOTE: You may remove this if you do not use Jekyll.
-apt-get -y install ruby-full build-essential
+apt-get -y install build-essential
+
+# Get ruby, as the version in apt is too old
+wget -O ruby-2.6.3.tar.gz https://cache.ruby-lang.org/pub/ruby/2.6/ruby-2.6.3.tar.gz
+tar -xzvf ruby-2.6.3.tar.gz
+cd ruby-2.6.3
+./configure
+make
+make install
+cd ..
+rm -rf ruby-2.6.3
+rm ruby-2.6.3.tar.gz
+
 echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
 echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
 echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
