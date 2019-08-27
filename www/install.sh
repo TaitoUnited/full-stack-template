70d32657f11962d460a48fde23101ecb398a85c5
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-08-27 21:29:41 +0300

    docs: improve www/install.sh instructions

--- www/install.sh
+++ www/install.sh
@@ -5,13 +5,21 @@ set -xe
 apt-get -y update
 cd /tmp
 
+#-------------------------------------------------------------------------
 # Install Hugo. NOTE: You may remove this if you do not use Hugo.
+#-------------------------------------------------------------------------
+
 HUGO_VERSION=0.54.0
 wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.deb
 apt install /tmp/hugo_extended_${HUGO_VERSION}_Linux-64bit.deb
 rm -f /tmp/hugo_extended_${HUGO_VERSION}_Linux-64bit.deb
 
-# Install Jekyll. NOTE: You may remove this if you do not use Jekyll.
+echo 'Hugo installed'
+
+#-------------------------------------------------------------------------
+# Install Jekyll. NOTE: You may remove all this if you do not use Jekyll
+#-------------------------------------------------------------------------
+
 apt-get -y install build-essential
 
 # Get ruby, as the version in apt is too old
@@ -30,3 +38,5 @@ echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
 echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
 . ~/.bashrc
 gem install jekyll bundler
+
+echo 'Jekyll installed'
