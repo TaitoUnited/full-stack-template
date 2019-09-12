f5b239bae38ea3a0d465f6ccacea22c26d0b8e0b
Jarkko Juslin <jarkko.juslin@gmail.com>
2019-09-12 12:04:08 +0300

    -Refactor flask app setup   -Fix db connection pool issues (pool being None on occasion)   -While running unit tests do not load config (and try to read secrets) -Disable logging on /healthz and /uptimez endpoints -Add error ids to logging and responses -Add db pool statistics to logging
    fixes #91

--- alternatives/server-flask/src/__init__.py
+++ alternatives/server-flask/src/__init__.py
@@ -1,15 +1,6 @@
 import typing
 from flask import Flask
 
-from . import config
-from . import db
-from . import log
-from . import posts
-from . import routes
-from . import sentry
-from . import storage
-from . import types # noqa
-
 
 def create_app(
     test_config: typing.Optional[typing.Mapping[str, typing.Any]] = None,
@@ -19,11 +10,13 @@ def create_app(
     app = Flask(__name__)
 
     if test_config is None:
+        from . import config
         app.config.from_object(config.Config)
     else:
         # load the test config if passed in
         app.config.from_object(test_config)
 
+    from . import log, sentry
     log.setup(app)
     sentry.connect(app)
 
@@ -31,15 +24,26 @@ def create_app(
     def handle_bad_request(e: Exception) -> typing.Any:
         """Catch-all exception handler.
         """
-        app.logger.exception(f'Unhandled exception: {e}')
-        return {'error': {'message': str(e)}}, 500
-
-    app.register_blueprint(routes.bp)
-    app.register_blueprint(posts.routes.bp)
+        # Create an id for the exception to make searching the logs
+        # easier.
+        e_id = id(e)
+        setattr(e, 'log_id', e_id)
+        app.logger.exception(f'Unhandled exception ({e_id}): {e}')
+        # All werkzeug raised exceptions (404 etc) have response code
+        # assigned to them.
+        code = getattr(e, 'code', 500)
+        return {'error': {'message': str(e), 'id': e_id}}, code
 
     def setup_worker() -> None:
+        # Connect infra
+        from . import db, storage
         db.connect(app)
         storage.connect(app)
+
+        # Register routes
+        from . import posts, routes
+        app.register_blueprint(routes.bp)
+        app.register_blueprint(posts.routes.bp)
 
     try:
         # If running behind uwsgi, init db as post fork op to avoid workers
