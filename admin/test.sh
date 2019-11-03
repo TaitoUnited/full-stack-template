87724d6a249c45239eb43bca6d29ec1b5969acb2
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2019-11-03 23:43:29 +0200

    chore: enable cypress tests on CI/CD

--- admin/test.sh
+++ admin/test.sh
@@ -5,13 +5,6 @@ export test_name="${2:-*}"
 
 case $suite_name in
   cypress)
-    if [ "$taito_mode" = "ci" ]; then
-      echo
-      echo "Cypress disabled on CI because of:"
-      echo "https://github.com/cypress-io/cypress-docker-images/issues/39"
-      echo
-    else
-      npm run cypress:run
-    fi
+    npm run cypress:run
     ;;
 esac
