40c0b045805e850ab44615ee65e5112ddd9114e8
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2020-07-06 17:24:23 +0300

    feat: replace placeholders on html files on startup

---
+++ www/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Replaces environment variables in **/*.html files. For example,
+# if REPLACE_CDN_PATH environment variable is defined, string 'CDN_PATH' is
+# replaced with REPLACE_CDN_PATH environment variable value in all html files.
+for NAME in $(awk "END { for (name in ENVIRON) { print name; }}" < /dev/null)
+do
+  case $NAME in REPLACE_*)
+    VAL="$(awk "END { printf ENVIRON[\"$NAME\"]; }" < /dev/null)"
+    CONSTANT="${NAME/REPLACE_/}"
+    REPLACEMENT="${VAL//\//\\/}"
+    TEMPFILE=$(mktemp)
+    find . -name '*.html' -exec sh -c \
+      "sed -e \"s/${CONSTANT}/${REPLACEMENT}/g\" {} > $TEMPFILE && cat $TEMPFILE > {}" \;
+  esac
+done
+
+exec "$@"
