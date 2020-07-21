1b86e1ae4c68579d8c0161c93e1861cccebd4f5f
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2020-07-22 00:22:38 +0300

    chore: remove obsolete stuff from koa.d.ts

--- server/src/types/koa.d.ts
+++ server/src/types/koa.d.ts
@@ -1,13 +1,7 @@
 // Additions by project
-import { IDatabase, ITask } from 'pg-promise';
 import { State } from '../common/types';
 
 declare module 'koa' {
-  interface Request {
-    body?: any;
-    params: { [key: string]: string };
-  }
-
   interface Context {
     state: State;
   }
