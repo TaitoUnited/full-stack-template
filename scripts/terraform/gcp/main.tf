8c7dcee71bf3be37ad0b0bcfd1fb989279bfd102
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2020-09-22 22:57:46 +0300

    feat(gcp): taito_ci_namespace_id

--- scripts/terraform/gcp/main.tf
+++ scripts/terraform/gcp/main.tf
@@ -23,9 +23,8 @@ locals {
 
 module "gcp" {
   source  = "TaitoUnited/project-resources/google"
-  version = "2.1.1"
+  version = "2.2.0"
 
-  create_build_trigger           = true
   create_storage_buckets         = true
   create_databases               = true
   create_in_memory_databases     = true
@@ -33,31 +32,35 @@ module "gcp" {
   create_service_accounts        = true
   create_service_account_roles   = true
   create_api_keys                = true
-  create_uptime_checks           = var.taito_uptime_provider == "gcp"
-  create_log_alert_metrics       = var.taito_logging_provider == "gcp"
-  create_log_alert_policies      = (
-    var.taito_logging_provider == "gcp" && var.taito_env == "prod"
-  )
 
-  # Provider
-  log_alert_project_id           = var.taito_logging_namespace_id
-  uptime_project_id              = var.taito_uptime_namespace_id
+  # Project
+  project                        = var.taito_project
+  env                            = var.taito_env
+
+  # Cloud provider
   project_id                     = var.taito_resource_namespace_id
   region                         = var.taito_provider_region
   zone                           = var.taito_provider_zone
 
-  # Labels
-  project                        = var.taito_project
-
-  # Environment info
-  env                            = var.taito_env
-
-  # Version control info
+  # Version control
   vc_repo                        = "${var.taito_vc_provider}_${var.taito_vc_organization}_${var.taito_vc_repository}"
   vc_branch                      = var.taito_branch
 
-  # Uptime
+  # CI/CD
+  create_build_trigger           = var.taito_ci_provider == "gcp"
+  cicd_project_id                = var.taito_ci_namespace_id
+
+  # Log monitoring
+  create_log_alert_metrics       = var.taito_logging_provider == "gcp"
+  create_log_alert_policies      = (
+    var.taito_logging_provider == "gcp" && var.taito_env == "prod"
+  )
+  log_alert_project_id           = var.taito_logging_namespace_id
+
+  # Uptime monitoring
+  create_uptime_checks           = var.taito_uptime_provider == "gcp"
   uptime_channels                = local.taito_uptime_channels
+  uptime_project_id              = var.taito_uptime_namespace_id
 
   # Additional resources as a json file
   resources                      = local.resources
--- scripts/terraform/gcp/variables.tf
+++ scripts/terraform/gcp/variables.tf
@@ -1,25 +1,3 @@
-# Provider
-
-variable "taito_provider_region" {
-  type = string
-}
-
-variable "taito_provider_zone" {
-  type = string
-}
-
-# Uptime provider
-
-variable "taito_uptime_provider" {
-  type = string
-  default = ""
-}
-
-variable "taito_uptime_channels" {
-  type = string  # whitespace delimited strings
-  default = ""
-}
-
 # Project
 
 variable "taito_project" {
@@ -30,7 +8,17 @@ variable "taito_env" {
   type = string
 }
 
-variable "taito_domain" {
+# Cloud provider
+
+variable "taito_resource_namespace_id" {
+  type = string
+}
+
+variable "taito_provider_region" {
+  type = string
+}
+
+variable "taito_provider_zone" {
   type = string
 }
 
@@ -52,74 +40,40 @@ variable "taito_branch" {
   type = string
 }
 
-# Namespaces
+# CI/CD
 
-variable "taito_namespace" {
+variable "taito_ci_provider" {
+  type = string
+  default = ""
+}
+
+variable "taito_ci_namespace_id" {
   type = string
 }
 
-variable "taito_resource_namespace_id" {
+# Logging
+
+variable "taito_logging_provider" {
   type = string
+  default = ""
 }
 
 variable "taito_logging_namespace_id" {
   type = string
 }
 
+# Uptime monitoring
+
+variable "taito_uptime_provider" {
+  type = string
+  default = ""
+}
+
 variable "taito_uptime_namespace_id" {
   type = string
 }
 
-# Storage
-
-variable "taito_storages" {
-  type = string  # whitespace delimited strings
-  default = ""
-}
-
-variable "taito_storage_locations" {
-  type = string  # whitespace delimited strings
-  default = ""
-}
-
-variable "taito_storage_classes" {
-  type = string  # whitespace delimited strings
-  default = ""
-}
-
-variable "taito_storage_days" {
-  type = string  # whitespace delimited strings
-  default = ""
-}
-
-variable "taito_storage_cors" {
-  type = string  # whitespace delimited strings
-  default = ""
-}
-
-variable "taito_storage_admins" {
-  type = string  # whitespace delimited strings
-  default = ""
-}
-
-variable "taito_storage_object_admins" {
-  type = string  # whitespace delimited strings
-  default = ""
-}
-
-variable "taito_storage_object_viewers" {
-  type = string  # whitespace delimited strings
-  default = ""
-}
-
-# Backup
-
-variable "taito_backup_locations" {
-  type = string  # whitespace delimited strings
-  default = ""
-}
-
-variable "taito_backup_days" {
+variable "taito_uptime_channels" {
   type = string  # whitespace delimited strings
   default = ""
 }
