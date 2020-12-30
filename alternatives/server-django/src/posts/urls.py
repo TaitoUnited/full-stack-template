1e50c4db4b9b700ff7dca8ce1960ee355528f101
Jf1nG <joonas.galab@hotmail.com>
2020-12-30 15:20:27 +0200

    fix: volume, clean  dockerfile, remove redundant file

--- alternatives/server-django/src/posts/urls.py
+++ alternatives/server-django/src/posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.posts, name='posts'),
+    path('<str:id>', views.post, name='post'),
 ]
