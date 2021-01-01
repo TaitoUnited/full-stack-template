25151141b74b49921712e0c915077a5ce838a746
Jf1nG <joonas.galab@hotmail.com>
2021-01-01 18:08:04 +0200

    fix: rename site to base (django conflict), remove formatters

--- alternatives/server-django/src/base/wsgi.py
+++ alternatives/server-django/src/base/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for site project.
+WSGI config for base project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
 
 application = get_wsgi_application()
--- alternatives/server-django/src/blog/apps.py
+++ alternatives/server-django/src/blog/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class PostsConfig(AppConfig):
-    name = 'posts'
+class BlogConfig(AppConfig):
+    name = 'blog'
