438d8ac3c53d6decccf5f716050656c53e3f37b4
keskiju <jukka.keskiluopa@taitounited.fi>
2021-03-08 11:55:35 +0200

    feat(django): fix test paths, remove config test

--- alternatives/server-django/test/test_infra.py
+++ alternatives/server-django/test/test_infra.py
@@ -1,19 +1,8 @@
-def test_get_config(client):
-    response = client.get('/config')
-    assert response.status_code == 200
-    assert response.headers['Content-Type'] == 'application/json'
-    config = response.json()
-    assert type(config) is dict
-    assert 'data' in config
-    assert type(config['data']) is dict
-    assert 'APP_VERSION' in config['data']
-
-
 def test_get_uptimez(client):
-    response = client.get('/uptimez')
+    response = client.get('/uptimez/')
     assert response.status_code == 200
 
 
 def test_get_healthz(client):
-    response = client.get('/healthz')
+    response = client.get('/healthz/')
     assert response.status_code == 200
