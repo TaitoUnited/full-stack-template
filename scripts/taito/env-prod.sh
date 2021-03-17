d07ad254b9d919561ae2db574d24069e6a261da9
keskiju <jukka.keskiluopa@taitounited.fi>
2021-03-17 13:00:28 +0200

    chore: use default_domain_prod in configs

--- scripts/taito/env-prod.sh
+++ scripts/taito/env-prod.sh
@@ -11,18 +11,18 @@ taito_basic_auth_enabled=true
 
 # Domain name (e.g. www.mydomain.com)
 taito_domain=
-taito_domain=$taito_project-$taito_target_env.${template_default_domain_prod} # TEMPLATE-REMOVE
+taito_domain=$taito_project-$taito_target_env.$default_domain_prod # TEMPLATE-REMOVE
 
 # Alternate domain name (e.g. mydomain.com) that redirects to main domain
 taito_altdomain=
 
 # Default domain name (copy this value to taito_domain if custom domain is not required)
-taito_default_domain=$taito_project-$taito_target_env.${template_default_domain_prod}
+taito_default_domain=$taito_project-$taito_target_env.$default_domain_prod
 
 # Canary environment domain name
 if [[ $taito_target_env == "canary" ]]; then
-  taito_domain=$taito_project-$taito_target_env.${template_default_domain_prod}
-  taito_default_domain=$taito_project-$taito_target_env.${template_default_domain_prod}
+  taito_domain=$taito_project-$taito_target_env.$default_domain_prod
+  taito_default_domain=$taito_project-$taito_target_env.$default_domain_prod
 fi
 
 # Temporarily allow building hotfix to prod by setting this to true
