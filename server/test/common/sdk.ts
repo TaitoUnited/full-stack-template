4545acac8ce00bdd9347e5febf6749df0b60c4db
keskiju <jukka.keskiluopa@taitounited.fi>
2021-06-02 13:17:18 +0300

    feat: add graphql code generator for tests

---
+++ server/test/common/sdk.ts
@@ -0,0 +1,93 @@
+import { GraphQLClient } from 'graphql-request';
+import * as Dom from 'graphql-request/dist/types.dom';
+import gql from 'graphql-tag';
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  /** The javascript `Date` as string. Type represents date and time as the ISO Date string. */
+  DateTime: any;
+};
+
+export type CreatePostInput = {
+  author: Scalars['String'];
+  content: Scalars['String'];
+  subject: Scalars['String'];
+};
+
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  /** Creates a new post. */
+  createPost: Post;
+};
+
+
+export type MutationCreatePostArgs = {
+  post: CreatePostInput;
+};
+
+export type Post = {
+  __typename?: 'Post';
+  author: Scalars['String'];
+  content: Scalars['String'];
+  createdAt: Scalars['DateTime'];
+  id: Scalars['String'];
+  subject: Scalars['String'];
+  updatedAt: Scalars['DateTime'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  /** Returns posts. */
+  posts: Array<Post>;
+};
+
+
+export type QueryPostsArgs = {
+  id?: Maybe<Scalars['String']>;
+};
+
+export type GetPostQueryVariables = Exact<{
+  id: Scalars['String'];
+}>;
+
+
+export type GetPostQuery = (
+  { __typename?: 'Query' }
+  & { posts: Array<(
+    { __typename?: 'Post' }
+    & Pick<Post, 'id' | 'subject'>
+  )> }
+);
+
+
+export const GetPostDocument = gql`
+    query getPost($id: String!) {
+  posts(id: $id) {
+    id
+    subject
+  }
+}
+    `;
+
+export type SdkFunctionWrapper = <T>(action: (requestHeaders?:Record<string, string>) => Promise<T>, operationName: string) => Promise<T>;
+
+
+const defaultWrapper: SdkFunctionWrapper = (action, _operationName) => action();
+
+export function getSdk(client: GraphQLClient, withWrapper: SdkFunctionWrapper = defaultWrapper) {
+  return {
+    getPost(variables: GetPostQueryVariables, requestHeaders?: Dom.RequestInit["headers"]): Promise<GetPostQuery> {
+      return withWrapper((wrappedRequestHeaders) => client.request<GetPostQuery>(GetPostDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'getPost');
+    }
+  };
+}
+export type Sdk = ReturnType<typeof getSdk>;
