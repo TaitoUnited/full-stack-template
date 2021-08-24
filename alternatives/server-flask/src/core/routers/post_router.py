cddaf813b8efbb8e98f51e2d97f135a862e63ff4
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-08-24 20:35:40 +0300

    refactor(server-flask): improve testability

--- alternatives/server-flask/src/core/routers/post_router.py
+++ alternatives/server-flask/src/core/routers/post_router.py
@@ -1,23 +1,25 @@
 import typing
 from flask import Blueprint, request
-from ..services import post_service
+from ..services.post_service import PostService
 from ..types.post import post_server_to_client, post_client_to_server
+
+post_service = PostService()
 
 
 bp = Blueprint("posts", __name__, url_prefix="/posts")
 
 
 @bp.route("")
-def get_all_posts() -> typing.Any:
+def search() -> typing.Any:
     """Get all posts."""
-    posts = post_service.get_all_posts()
+    posts = post_service.search()
     return {"data": post_server_to_client(posts)}
 
 
 @bp.route("", methods=("POST",))
-def create_post() -> typing.Any:
+def create() -> typing.Any:
     """Save new post to database."""
     request_data = request.get_json()
     post_data = post_client_to_server(request_data["data"])
-    post = post_service.create_post(post_data)
+    post = post_service.create(post_data)
     return {"data": post_server_to_client(post)}, 201
--- alternatives/server-flask/src/core/services/post_service.py
+++ alternatives/server-flask/src/core/services/post_service.py
@@ -1,11 +1,14 @@
 import typing
 from ..types.post import DBPost, Post
-from ..daos import post_dao
+from ..daos.post_dao import PostDao
 
 
-def get_all_posts() -> typing.List[DBPost]:
-    return post_dao.get_all_posts()
+class PostService:
+    def __init__(self, post_dao=None):
+        self._post_dao = post_dao or PostDao()
 
+    def search(self) -> typing.List[DBPost]:
+        return self._post_dao.search()
 
-def create_post(data: Post) -> DBPost:
-    return post_dao.create_post(data)
+    def create(self, data: Post) -> DBPost:
+        return self._post_dao.create(data)
