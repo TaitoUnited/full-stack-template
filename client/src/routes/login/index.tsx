99130b515a39c7fd95c1ebac02c004c1bf0bd424
Teemu Taskula <themetor@gmail.com>
2021-09-06 15:36:48 +0300

    fix(client): fix login screen content centering

--- client/src/routes/login/index.tsx
+++ client/src/routes/login/index.tsx
@@ -68,6 +68,7 @@ export default function LoginPage() {
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
+  min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
