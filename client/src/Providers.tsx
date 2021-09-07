c35df599f7cf8f5679c1b5a36dec3a835d4411af
Teemu Taskula <themetor@gmail.com>
2021-09-07 11:15:45 +0300

    feat(client): persist apollo cache

--- client/src/Providers.tsx
+++ client/src/Providers.tsx
@@ -1,27 +1,23 @@
 import type { ReactNode } from 'react';
 import { OverlayProvider } from 'react-aria';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/client';
 
 import Toaster from '~components/common/Toaster';
 import { ThemingProvider } from '~services/theming';
 import { I18nProvider } from '~services/i18n';
 import { AuthProvider } from '~services/auth';
-import { apolloClient } from '~graphql';
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
-    <ApolloProvider client={apolloClient}>
-      <ThemingProvider>
-        <AuthProvider>
-          <I18nProvider>
-            <OverlayProvider>
-              <Router>{children}</Router>
-            </OverlayProvider>
-            <Toaster />
-          </I18nProvider>
-        </AuthProvider>
-      </ThemingProvider>
-    </ApolloProvider>
+    <ThemingProvider>
+      <AuthProvider>
+        <I18nProvider>
+          <OverlayProvider>
+            <Router>{children}</Router>
+          </OverlayProvider>
+          <Toaster />
+        </I18nProvider>
+      </AuthProvider>
+    </ThemingProvider>
   );
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,27 @@
 import './index.css';
 
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from '@apollo/client';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { setupErrorReporting } from '~services/reporting';
+import { setupApolloClient } from '~graphql';
 
-setupErrorReporting();
+async function init() {
+  setupErrorReporting();
 
-ReactDOM.render(<App />, document.getElementById('app'));
+  const apolloClient = await setupApolloClient();
+
+  ReactDOM.render(
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>,
+    document.getElementById('app')
+  );
+}
+
+init();
 
 // NOTE: Service Worker is enabled only in production builds
 // NOTE: check `serviceWorker.ts` if you want to use automatic app update propmt
