629d5eda9cd3053e271b8784bf142a5391d4b639
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-10-07 15:30:44 +0300

    chore: update azure terraform module

--- scripts/terraform/azure/main.tf
+++ scripts/terraform/azure/main.tf
@@ -145,7 +145,7 @@ locals {
 
 module "azure" {
   source  = "TaitoUnited/project-resources/azurerm"
-  version = "3.3.2"
+  version = "3.4.1"
 
   # Create flags
   create_cicd_service_account         = var.create_cicd_service_account
