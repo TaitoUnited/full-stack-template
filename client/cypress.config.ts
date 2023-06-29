38e908d484b68476293ec4c09137cb00f62a8cd1
Teemu Taskula <themetor@gmail.com>
2023-06-29 13:54:29 +0300

    chore(client): add info about cypress setup

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'https://this-will-be-provided-by-taito-cli.com',
+    // NOTE: when you run the tests via taito-cli (`taito cypress:client`)
+    // the baseUrl is automatically set to the correct value.
+    baseUrl: 'http://localhost:8080',
     specPattern: 'test/**/*.spec.ts',
     fixturesFolder: 'test/fixtures',
     screenshotsFolder: 'test/screenshots',
