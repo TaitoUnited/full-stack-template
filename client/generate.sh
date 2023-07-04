01b1b474160092beb15f8259d0260249d9ed9e9b
keskiju <jukka.keskiluopa@taitounited.fi>
2023-07-04 12:15:27 +0300

    feat(client): run lang:extract on generate

--- client/generate.sh
+++ client/generate.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 echo
+echo "Extracting translations from source code"
+echo "Compilation is done automatically by vite lingui plugin"
+npm run lang:extract
+
+echo
 echo "Generating GraphQL code for client"
 npm run generate:graphql
