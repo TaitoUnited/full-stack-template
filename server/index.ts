26495bca4be500d557371d3e3740a1f8ae029308
keskiju <jukka.keskiluopa@taitounited.fi>
2023-10-06 11:11:23 +0300

    feat(server): add module alias register

--- server/index.ts
+++ server/index.ts
@@ -1,2 +1,3 @@
+import 'module-alias/register';
 import * as service from './src/server';
 export const handler = service.handler;
--- server/src/cli.ts
+++ server/src/cli.ts
@@ -1,3 +1,4 @@
+import 'module-alias/register';
 import 'reflect-metadata';
 import { Container } from 'typedi';
 import getDb from './common/setup/db';
