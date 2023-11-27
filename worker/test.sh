18f7b36b6956e227c61f7c6f4561b68c810c6f82
keskiju <jukka.keskiluopa@taitounited.fi>
2023-11-27 19:44:14 +0200

    feat: db proxy healthcheck instead of sleep

--- worker/test.sh
+++ worker/test.sh
@@ -3,11 +3,6 @@
 export suite_name="${1}"
 export test_name="${2}"
 
-if [ "$TEST_ENV_REMOTE" = "true" ]; then
-  echo "Waiting for db proxy to connect"
-  sleep 10
-fi
-
 case $suite_name in
   jest)
     npm run test -- "${test_name}"
