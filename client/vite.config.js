b64f4a1d041516f18e4ac9fc687d9d4e04637821
Teemu Taskula <themetor@gmail.com>
2024-05-21 13:40:00 +0300

    fix(client): remove replace swc with babel
    Using SWC is unfortunately extremely painful as the Rust bindings
    are too easy to get messed up if the different SWC plugin versions
    don't match perfectly.
    
    This revert the client to use Babel again as that is more robust and it
    should be fast enough for most projects.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
 import path from 'path';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import strip from '@rollup/plugin-strip';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import pandaCss from '@pandacss/dev/postcss';
@@ -82,8 +82,7 @@ export default defineConfig(({ mode }) => ({
     taitoHtmlFragmentsPlugin(),
     react({
       exclude: /\.stories\.(t|j)sx?$/, // Exclude Storybook stories
-      plugins: [['@lingui/swc-plugin', {}]],
-      jsxRuntime: mode === 'development' ? 'automatic' : 'classic',
+      babel: { plugins: ['macros'] },
     }),
     lingui(),
   ].filter(Boolean),
