43c8e95cccaa4e9911c1e4d3c2710d68d7748850
Teemu Taskula <themetor@gmail.com>
2024-05-23 15:27:22 +0300

    fix(client): fix uikit skeleton placeholder story

--- client/src/components/uikit/SkeletonPlaceholder/SkeletonPlaceholder.stories.tsx
+++ client/src/components/uikit/SkeletonPlaceholder/SkeletonPlaceholder.stories.tsx
@@ -1,37 +1,37 @@
-import { SkeletonPlaceholder } from './index';
-import { styled } from '~styled-system/jsx';
-import { Stack } from '~uikit';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Stack, SkeletonPlaceholder } from '~uikit';
 
 export default {
   title: 'SkeletonPlaceholder',
   component: SkeletonPlaceholder,
-};
+  decorators: [
+    Story => (
+      <div style={{ maxWidth: '600px' }}>
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof SkeletonPlaceholder>;
 
-export function Example() {
-  return (
-    <Wrapper>
-      <Stack direction="column" gap="$xl">
-        <SkeletonPlaceholder width={400} height={40} borderRadius="small" />
+type Story = StoryObj<typeof SkeletonPlaceholder>;
 
-        <Stack direction="row" gap="$xl">
-          <SkeletonPlaceholder width={200} height={200} borderRadius="full" />
-          <Stack direction="column" gap="$large" style={{ flexGrow: 1 }}>
-            <SkeletonPlaceholder height={200} borderRadius="regular" />
-            <SkeletonPlaceholder
-              width={500}
-              height={100}
-              borderRadius="regular"
-            />
-          </Stack>
+export const Example: Story = {
+  render: () => (
+    <Stack direction="column" gap="$xl">
+      <SkeletonPlaceholder width={400} height={40} borderRadius="small" />
+
+      <Stack direction="row" gap="$xl">
+        <SkeletonPlaceholder width={200} height={200} borderRadius="full" />
+        <Stack direction="column" gap="$large" style={{ flexGrow: 1 }}>
+          <SkeletonPlaceholder height={200} borderRadius="regular" />
+          <SkeletonPlaceholder
+            width={500}
+            height={100}
+            borderRadius="regular"
+          />
         </Stack>
       </Stack>
-    </Wrapper>
-  );
-}
-
-const Wrapper = styled('div', {
-  base: {
-    backgroundColor: '$surface',
-    padding: '$large',
-  },
-});
+    </Stack>
+  ),
+};
--- client/src/components/uikit/SkeletonPlaceholder/index.tsx
+++ client/src/components/uikit/SkeletonPlaceholder/index.tsx
@@ -4,7 +4,7 @@ import './styles.css';
 
 import { token } from '~styled-system/tokens';
 import { styled } from '~styled-system/jsx';
-import { Color, Spacing, Radii } from '~design-tokens/types';
+import type { Color, Spacing, Radii } from '~design-tokens/types';
 
 type Props = {
   marginTop?: Spacing | number;
@@ -19,14 +19,14 @@ type Props = {
 };
 
 export function SkeletonPlaceholder({
+  style,
   width,
   height,
-  marginTop,
-  marginRight,
-  marginBottom,
-  marginLeft,
-  borderRadius,
-  style,
+  marginTop: mt,
+  marginRight: mr,
+  marginBottom: mb,
+  marginLeft: ml,
+  borderRadius: br,
   ...rest
 }: Props) {
   return (
@@ -34,11 +34,11 @@ export function SkeletonPlaceholder({
       style={{
         width,
         height,
-        marginTop: typeof marginTop === 'string' ? token.var(`$spacing.${marginTop}`) : marginTop, // prettier-ignore
-        marginRight: typeof marginRight === 'string' ? token.var(`$spacing.${marginRight}`) : marginRight, // prettier-ignore
-        marginBottom: typeof marginBottom === 'string' ? token.var(`$spacing.${marginBottom}`) : marginBottom, // prettier-ignore
-        marginLeft: typeof marginLeft === 'string' ? token.var(`$spacing.${marginLeft}`) : marginLeft, // prettier-ignore
-        borderRadius: typeof borderRadius === 'string' ? token.var(`$radii.${borderRadius}`) : borderRadius, // prettier-ignore
+        marginTop: typeof mt === 'string' ? token.var(`$spacing.${mt}`) : mt,
+        marginRight: typeof mr === 'string' ? token.var(`$spacing.${mr}`) : mr,
+        marginBottom: typeof mb === 'string' ? token.var(`$spacing.${mb}`) : mb,
+        marginLeft: typeof ml === 'string' ? token.var(`$spacing.${ml}`) : ml,
+        borderRadius: typeof br === 'string' ? token.var(`$radii.${br}`) : br,
         flexGrow: width ? 0 : 1,
         ...style,
       }}
