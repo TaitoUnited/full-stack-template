d98747720fc327680399cb16d31ae60c64a07be8
Teemu Taskula <themetor@gmail.com>
2024-05-23 15:29:43 +0300

    fix(client): fix uikit spinner story

--- client/src/components/uikit/Spinner/Spinner.stories.tsx
+++ client/src/components/uikit/Spinner/Spinner.stories.tsx
@@ -1,29 +1,40 @@
-import { Stack, Spinner } from '~uikit';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Spinner, Stack } from '~uikit';
 
 export default {
   title: 'Spinner',
   component: Spinner,
-};
+  decorators: [
+    Story => (
+      <div style={{ maxWidth: '300px' }}>
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof Spinner>;
+
+type Story = StoryObj<typeof Spinner>;
 
 const colors = ['primary', 'text', 'info', 'warn', 'error'] as const;
 
-export function Colors() {
-  return (
+export const Color: Story = {
+  render: () => (
     <Stack direction="row" gap="$regular">
       {colors.map(color => (
-        <Spinner key={color} color={color} size="large" />
+        <Spinner key={color} color={color} size="medium" />
       ))}
     </Stack>
-  );
-}
+  ),
+};
 
-export function Sizes() {
-  return (
+export const Sizes: Story = {
+  render: () => (
     <Stack direction="row" gap="$large">
       <Spinner color="primary" size="small" />
       <Spinner color="primary" size="normal" />
       <Spinner color="primary" size="medium" />
       <Spinner color="primary" size="large" />
     </Stack>
-  );
-}
+  ),
+};
