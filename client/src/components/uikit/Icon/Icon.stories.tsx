4e2dd241e61f987208a881b7666fed6a9769e8c7
Teemu Taskula <themetor@gmail.com>
2024-05-23 15:37:37 +0300

    fix(client): fix uikit icon story

--- client/src/components/uikit/Icon/Icon.stories.tsx
+++ client/src/components/uikit/Icon/Icon.stories.tsx
@@ -1,19 +1,40 @@
-import { Stack, Tooltip, Icon } from '~uikit';
+import type { Meta, StoryObj } from '@storybook/react';
+
 import { ids } from '~design-tokens/icon-sprite-ids';
+import { Tooltip, Icon } from '~uikit';
 
 export default {
   title: 'Icon',
   component: Icon,
-};
+} satisfies Meta<typeof Icon>;
 
-export function AllIcons() {
-  return (
-    <Stack direction="row" gap="$large" style={{ flexWrap: 'wrap' }}>
+type Story = StoryObj<typeof Icon>;
+
+export const All: Story = {
+  render: () => (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fill, minmax(60px, 1fr))',
+        gap: 20,
+        padding: 20,
+      }}
+    >
       {ids.map((name, i) => (
         <Tooltip content={name} key={i}>
-          <Icon name={name} size={24} color="text" />
+          <Tooltip.Trigger>
+            <div
+              style={{
+                display: 'flex',
+                placeContent: 'center',
+                padding: 4,
+              }}
+            >
+              <Icon name={name} size={24} color="text" />
+            </div>
+          </Tooltip.Trigger>
         </Tooltip>
       ))}
-    </Stack>
-  );
-}
+    </div>
+  ),
+};
