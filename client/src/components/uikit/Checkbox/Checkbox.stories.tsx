201a52dc3fc830452195d216379d36cd35ed50ef
Teemu Taskula <themetor@gmail.com>
2024-05-23 15:44:53 +0300

    fix(client): fix uikit checkbox stories

--- client/src/components/uikit/Checkbox/Checkbox.stories.tsx
+++ client/src/components/uikit/Checkbox/Checkbox.stories.tsx
@@ -1,49 +1,80 @@
-import { useState } from 'react';
-import { Stack, Text, Checkbox } from '~uikit';
+import type { Meta, StoryObj } from '@storybook/react';
+import { css } from '~styled-system/css';
+
+import { Stack, Checkbox, Text } from '~uikit';
 
 export default {
   title: 'Checkbox',
   component: Checkbox,
+  decorators: [
+    Story => (
+      <div style={{ maxWidth: '600px' }}>
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof Checkbox>;
+
+type Story = StoryObj<typeof Checkbox>;
+
+export const Regular: Story = {
+  args: {
+    label: 'Regular checkbox',
+  },
 };
 
-export const Example = () => {
-  const [isChecked, setChecked] = useState(false);
+export const ComponentLabel: Story = {
+  args: {
+    label: <Text variant="bodyBold">Styled label label</Text>,
+  },
+};
 
-  return (
-    <div style={{ padding: 32 }}>
-      <Stack direction="column" gap="$large">
-        <Checkbox
-          isSelected={isChecked}
-          onChange={setChecked}
-          label="Sluibs?"
-        />
+export const HiddenLabel: Story = {
+  render: () => (
+    <Stack direction="column" gap="$small">
+      <Text variant="body">(This checkbox has a hidden label)</Text>
+      <Checkbox hiddenLabel="Testing" />
+    </Stack>
+  ),
+};
 
-        <Checkbox label={<Text variant="bodyBold">Styled label label</Text>} />
+export const OuterLabel: Story = {
+  render: () => (
+    <Stack direction="column" gap="$small">
+      <Text id="outer-label" variant="body">
+        This label is connected via the `labelledby` prop
+      </Text>
+      <Checkbox labelledby="outer-label" />
+    </Stack>
+  ),
+};
 
-        <Stack direction="column" gap="$small">
-          <Text id="outer-label" variant="body">
-            Outer label
-          </Text>
-          <Checkbox labelledby="outer-label" />
-        </Stack>
+export const Disabled: Story = {
+  args: {
+    label: 'Disabled checkbox',
+    isDisabled: true,
+  },
+};
 
-        <Stack direction="column" gap="$small">
-          <Text variant="body">(This checkbox has a hidden label)</Text>
-          <Checkbox hiddenLabel="Testing" />
-        </Stack>
+export const Invalid: Story = {
+  args: {
+    label: 'Invalid checkbox',
+    isInvalid: true,
+  },
+};
 
-        <Checkbox isDisabled label="I'm disabled" />
-
-        <Checkbox isInvalid label="I'm invalid" />
-
-        <Stack direction="column" gap="$small">
-          <Checkbox isIndeterminate label="I'm not sure about myself" />
-          <Stack direction="column" gap="$xs" style={{ paddingLeft: 20 }}>
-            <Checkbox isSelected label="Child checkbox 1" />
-            <Checkbox label="Child checkbox 2" />
-          </Stack>
-        </Stack>
+export const Indeterminate: Story = {
+  render: () => (
+    <Stack direction="column" gap="$small">
+      <Checkbox isIndeterminate label="I'm not sure about myself" />
+      <Stack
+        direction="column"
+        gap="$xs"
+        className={css({ paddingLeft: '$regular' })}
+      >
+        <Checkbox isSelected label="Child checkbox 1" />
+        <Checkbox isSelected={false} label="Child checkbox 2" />
       </Stack>
-    </div>
-  );
+    </Stack>
+  ),
 };
