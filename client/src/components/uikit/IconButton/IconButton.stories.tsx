0848a9b05f8ac90e455ccf4701c9fd55983f4d37
Teemu Taskula <themetor@gmail.com>
2024-05-29 16:37:44 +0300

    feat(client): add icon button variants

--- client/src/components/uikit/IconButton/IconButton.stories.tsx
+++ client/src/components/uikit/IconButton/IconButton.stories.tsx
@@ -10,23 +10,95 @@ export default {
 type Story = StoryObj<typeof IconButton>;
 
 const variants = ['filled', 'outlined', 'soft', 'plain'] as const;
-const colors = ['primary', 'error', 'success'] as const;
+const colors = ['primary', 'error', 'success', 'neutral'] as const;
+const sizes = ['small', 'medium', 'large'] as const;
+const tooltipPlacements = ['top', 'bottom', 'left', 'right'] as const;
 
-export const Variations: Story = {
+export const Variant: Story = {
   render: () => (
-    <Stack direction="row" gap="medium">
-      {variants.map(variant => (
-        <Stack key={variant} direction="row" gap="regular" align="flex-start">
-          {colors.map(color => (
+    <Stack direction="column" gap="medium">
+      {colors.map(color => (
+        <Stack key={color} direction="row" gap="regular">
+          {variants.map(variant => (
             <IconButton
-              key={color}
+              key={variant}
+              variant={variant}
               color={color}
-              label="Icon button"
-              icon="bellFilled"
+              label={`Icon button ${variant}`}
+              icon="document"
             />
           ))}
         </Stack>
       ))}
     </Stack>
   ),
+};
+
+export const Color: Story = {
+  render: () => (
+    <Stack direction="row" gap="regular">
+      {colors.map(color => (
+        <IconButton
+          key={color}
+          variant="plain"
+          color={color}
+          label={`Icon button ${color}`}
+          icon="document"
+        />
+      ))}
+    </Stack>
+  ),
+};
+
+export const Size: Story = {
+  render: () => (
+    <Stack direction="column" gap="medium">
+      {sizes.map(size => (
+        <Stack key={size} direction="row" gap="regular">
+          {variants.map(variant => (
+            <IconButton
+              key={variant}
+              variant={variant}
+              size={size}
+              label={`Icon button ${size}`}
+              icon="filter"
+            />
+          ))}
+        </Stack>
+      ))}
+    </Stack>
+  ),
+};
+
+export const Tooltip: Story = {
+  render: () => (
+    <Stack direction="column" gap="medium">
+      {tooltipPlacements.map(tooltipPlacement => (
+        <Stack key={tooltipPlacement} direction="row" gap="regular">
+          {variants.map(variant => (
+            <IconButton
+              key={variant}
+              variant={variant}
+              tooltipPlacement={tooltipPlacement}
+              label="Icon button"
+              icon="fullscreenOpen"
+            />
+          ))}
+        </Stack>
+      ))}
+    </Stack>
+  ),
+  decorators: [
+    Story => (
+      <div
+        style={{
+          padding: '50px',
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
 };
--- client/src/components/uikit/IconButton/index.tsx
+++ client/src/components/uikit/IconButton/index.tsx
@@ -1,19 +1,30 @@
-import { forwardRef } from 'react';
+import { CSSProperties, forwardRef } from 'react';
 import { Button, ButtonProps } from 'react-aria-components';
 
 import { Tooltip } from '../Tooltip';
 import { Icon, IconName } from '../Icon';
-import { css, cx } from '~styled-system/css';
-import { StyledSystemToken } from '~utils/styled-system';
-import { ColorToken } from '~styled-system/tokens';
+import { RecipeVariantProps, cva, cx } from '~styled-system/css';
+import { token } from '~styled-system/tokens';
 
-type Props = ButtonProps & {
+type IconButtonSize = 'small' | 'medium' | 'large';
+
+type IconButtonColor = 'primary' | 'success' | 'error' | 'neutral';
+
+type OwnProps = {
   icon: IconName;
   label: string;
-  size?: number;
-  color?: StyledSystemToken<ColorToken>;
+  size?: IconButtonSize | number;
+  color?: IconButtonColor;
   tooltipPlacement?: 'top' | 'bottom' | 'left' | 'right';
   className?: string;
+};
+
+type Props = ButtonProps & RecipeVariantProps<typeof styles> & OwnProps;
+
+const sizeEnumToNumber: { [size in IconButtonSize]: number } = {
+  small: 32,
+  medium: 36,
+  large: 44,
 };
 
 export const IconButton = forwardRef<HTMLButtonElement, Props>(
@@ -21,47 +32,117 @@ export const IconButton = forwardRef<HTMLButtonElement, Props>(
     {
       label,
       icon,
+      style,
       className,
-      color = 'text',
-      size = 32,
+      color = 'neutral',
+      size = 'medium',
+      variant = 'plain',
       tooltipPlacement = 'top',
       ...rest
     },
     ref
   ) => {
+    let colorMuted: string;
+
+    if (color === 'neutral') {
+      colorMuted = token.var('$colors.neutral5');
+    } else {
+      colorMuted = token.var(`$colors.${color}Muted`);
+    }
+
+    let colorText: string;
+
+    if (color === 'neutral') {
+      colorText = token.var('$colors.text');
+    } else {
+      colorText = token.var(`$colors.${color}Contrast`);
+    }
+
+    let colorValue: string;
+
+    if (color === 'neutral') {
+      colorValue = token.var('$colors.neutral1');
+    } else {
+      colorValue = token.var(`$colors.${color}`);
+    }
+
+    const _className = cx(styles({ variant }), className);
+
+    const _size = typeof size === 'number' ? size : sizeEnumToNumber[size];
+
+    const _style = {
+      ...style,
+      width: _size,
+      height: _size,
+      '--color-muted': colorMuted,
+      '--color-text': colorText,
+      '--color': colorValue,
+    } as CSSProperties;
+
     return (
       <Tooltip content={label} placement={tooltipPlacement}>
-        <Button
-          {...rest}
-          ref={ref}
-          className={cx(styles, className, css({ width: size, height: size }))}
-        >
-          <Icon name={icon} size={size * 0.75} color={color} />
+        <Button ref={ref} style={_style} className={_className} {...rest}>
+          <Icon name={icon} size={_size * 0.6} color="currentColor" />
         </Button>
       </Tooltip>
     );
   }
 );
 
-const styles = css({
-  $focusRing: true,
-  $hoverHighlight: true,
-  position: 'relative',
-  margin: 0,
-  borderRadius: '50%',
-  textDecoration: 'none',
-  cursor: 'pointer',
-  display: 'inline-flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+IconButton.displayName = 'IconButton';
 
-  '&:active': {
-    opacity: 0.8,
+const styles = cva({
+  base: {
+    position: 'relative',
+    margin: 0,
+    borderRadius: '50%',
+    textDecoration: 'none',
+    cursor: 'pointer',
+    display: 'inline-flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    outlineOffset: '2px',
+
+    '&[data-pressed="true"]': {
+      opacity: 0.8,
+    },
+    '&[data-disabled="true"]': {
+      opacity: 0.5,
+      cursor: 'not-allowed',
+    },
+    '&[data-focus-visible="true"]': {
+      outline: '2px solid var(--color)',
+    },
   },
-  '&:disabled': {
-    opacity: 0.5,
-    cursor: 'not-allowed',
+  variants: {
+    variant: {
+      filled: {
+        backgroundColor: 'var(--color)',
+        color: 'white',
+        $hoverHighlight: true,
+      },
+      soft: {
+        color: 'var(--color-text)',
+        backgroundColor: 'var(--color-muted)',
+        $hoverHighlight: true,
+      },
+      outlined: {
+        backgroundColor: 'transparent',
+        color: 'var(--color-text)',
+        borderColor: 'var(--color)',
+        borderWidth: '1px',
+        borderStyle: 'solid',
+        '&[data-hovered="true"]': {
+          backgroundColor: 'var(--color-muted)',
+        },
+      },
+      plain: {
+        backgroundColor: 'transparent',
+        color: 'var(--color-text)',
+        '&[data-hovered="true"]': {
+          backgroundColor: 'var(--color-muted)',
+        },
+      },
+    },
   },
 });
-
-IconButton.displayName = 'IconButton';
