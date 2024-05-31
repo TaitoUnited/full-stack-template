1f3a3fd2ad37ae0c76502cb85716a5c5c338b6d6
Teemu Taskula <themetor@gmail.com>
2024-05-31 11:04:06 +0300

    chore(client): update sentry

--- client/src/services/reporting.ts
+++ client/src/services/reporting.ts
@@ -1,10 +1,10 @@
-import * as Sentry from '@sentry/browser';
+import { init } from '@sentry/browser';
 import { config } from '~constants/config';
 
 export const setupErrorReporting = () => {
   if (config.ERROR_REPORTING_ENABLED) {
-    Sentry.init({ dsn: config.SENTRY_DSN });
+    init({ dsn: config.SENTRY_DSN });
   } else {
-    console.log('> Ignoring error reporting setup for this environment');
+    console.log('Ignoring error reporting setup for this environment');
   }
 };
