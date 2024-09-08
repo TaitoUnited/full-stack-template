b04f1b8ac7ab2f6e51b38d4fdfbaee8eb735dca9
keskiju <jukka.keskiluopa@taitounited.fi>
2024-09-09 00:39:30 +0300

    feat: use gcp terraform module v3.11.0

--- scripts/terraform/gcp/main.tf
+++ scripts/terraform/gcp/main.tf
@@ -146,7 +146,7 @@ locals {
 
 module "gcp" {
   source  = "TaitoUnited/project-resources/google"
-  version = "3.10.0"
+  version = "3.11.0"
 
   create_cicd_service_account    = var.create_cicd_service_account
   create_storage_buckets         = true
