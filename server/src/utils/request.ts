9148260f3489dc74b04cb426aa3b11fb46598617
Teemu Taskula <themetor@gmail.com>
2024-09-12 10:46:09 +0300

    fix: string header type casting

--- server/src/utils/request.ts
+++ server/src/utils/request.ts
@@ -2,6 +2,6 @@ import { FastifyRequest } from 'fastify';
 
 export function getStringHeader(request: FastifyRequest, header: string) {
   return typeof request.headers[header] === 'string'
-    ? request.headers[header]
+    ? (request.headers[header] as string)
     : null;
 }
