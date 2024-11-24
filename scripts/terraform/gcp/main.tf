2d44dda2e03298445aa44671f42a6d0ca5a8327a
keskiju <jukka.keskiluopa@taitounited.fi>
2024-11-24 18:08:49 +0200

    chore(gcp): terraform module 3.14.0

--- scripts/terraform/gcp/main.tf
+++ scripts/terraform/gcp/main.tf
@@ -146,7 +146,7 @@ locals {
 
 module "gcp" {
   source  = "TaitoUnited/project-resources/google"
-  version = "3.13.1"
+  version = "3.14.0"
 
   create_container_image_repositories = var.taito_env == "dev"
 
