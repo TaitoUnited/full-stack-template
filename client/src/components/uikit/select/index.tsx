9429b0b86230c19704d33544461b3de6d50842d3
Julien Texier <julien.texier@taitounited.fi>
2025-01-24 11:10:33 +0200

    feat(client): enhance Select component

--- client/src/components/uikit/select/index.tsx
+++ client/src/components/uikit/select/index.tsx
@@ -1,4 +1,5 @@
-import { type ComponentProps } from 'react';
+import { type Ref } from 'react';
+import { type AriaSelectProps } from 'react-aria';
 import {
   Select as AriaSelect,
   Button,
@@ -12,9 +13,9 @@ import {
 import { css, cx } from '~/styled-system/css';
 
 import { Icon, type IconName } from '../icon';
+import { type FormComponentProps } from '../partials/common';
 import {
-  DescriptionText,
-  ErrorText,
+  FormInputContainer,
   inputBaseStyles,
   inputIconLeftStyles,
   inputIconRightStyles,
@@ -22,6 +23,7 @@ import {
   labelStyles,
   listBoxItemStyles,
   listBoxStyles,
+  useInputContext,
 } from '../partials/common';
 import { SelectItem } from '../partials/select-item';
 
@@ -31,12 +33,11 @@ export type SelectOption = {
   description?: string;
 };
 
-type Props = ComponentProps<typeof AriaSelect<SelectOption>> & {
-  label: string;
+type Props = FormComponentProps<
+  Omit<AriaSelectProps<SelectOption>, 'children'>
+> & {
+  ref?: Ref<HTMLDivElement>;
   items: SelectOption[];
-  description?: string;
-  /** Passing an `errorMessage` as prop toggles the input as invalid. */
-  errorMessage?: string;
   icon?: IconName;
 };
 
@@ -48,58 +49,74 @@ type Props = ComponentProps<typeof AriaSelect<SelectOption>> & {
 export function Select({
   ref,
   label,
+  labelledby,
+  hiddenLabel,
+  labelPosition: labelPositionProp,
   description,
   errorMessage,
   icon,
   items,
+  value,
+  onChange,
   ...rest
 }: Props) {
+  const inputContext = useInputContext();
+  const labelPosition = labelPositionProp ?? inputContext.labelPosition;
+
   return (
     <AriaSelect
       {...rest}
+      aria-labelledby={labelledby}
+      aria-label={hiddenLabel}
       ref={ref}
       isInvalid={!!errorMessage}
-      className={cx(inputWrapperStyles, rest.className as string)}
+      className={cx(inputWrapperStyles({ labelPosition }), rest.className)}
+      selectedKey={value}
+      onSelectionChange={val => onChange?.(val as string)}
     >
-      <Label className={labelStyles} data-required={rest.isRequired}>
-        {label}
-      </Label>
-
-      <div className={css({ position: 'relative' })}>
-        {!!icon && (
-          <Icon
-            name={icon}
-            size={20}
-            color="neutral1"
-            className={inputIconLeftStyles}
-          />
-        )}
-
-        <Button
-          data-invalid={!!errorMessage}
-          data-has-icon={!!icon}
-          className={cx(
-            inputBaseStyles,
-            css({
-              paddingRight: '$xl!',
-              '&[data-has-icon="true"]': { paddingLeft: '$xl' },
-              '& > *[data-placeholder]': { color: '$textMuted' },
-            })
-          )}
+      {!!label && (
+        <Label
+          className={labelStyles({ labelPosition })}
+          data-required={rest.isRequired}
         >
-          <SelectValue />
-        </Button>
+          {label}
+        </Label>
+      )}
 
-        <Icon
-          name="arrowDropDown"
-          size={24}
-          color="text"
-          className={cx(inputIconRightStyles, css({ right: '$xs!' }))}
-        />
-      </div>
+      <FormInputContainer description={description} errorMessage={errorMessage}>
+        <div className={css({ position: 'relative' })}>
+          {!!icon && (
+            <Icon
+              name={icon}
+              size={20}
+              color="neutral1"
+              className={inputIconLeftStyles}
+            />
+          )}
 
-      {!!description && <DescriptionText>{description}</DescriptionText>}
-      {!!errorMessage && <ErrorText>{errorMessage}</ErrorText>}
+          <Button
+            data-invalid={!!errorMessage}
+            data-has-icon={!!icon}
+            className={cx(
+              inputBaseStyles,
+              css({
+                paddingRight: '$xl!',
+                '&[data-has-icon="true"]': { paddingLeft: '$xl' },
+                '& > *[data-placeholder]': { color: '$textMuted' },
+              })
+            )}
+          >
+            <SelectValue />
+          </Button>
+
+          <Icon
+            name="arrowDropDown"
+            size={24}
+            color="text"
+            className={cx(inputIconRightStyles, css({ right: '$xs!' }))}
+          />
+        </div>
+      </FormInputContainer>
 
       <Popover offset={4}>
         <ListBox className={listBoxStyles} items={items}>
