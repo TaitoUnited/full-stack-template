d2bcfbbafdb46ae34ce32d5c94a149504236a98c
Leevi Parkkinen <48884966+slebes@users.noreply.github.com>
2025-09-04 11:22:31 +0300

    chore: update package json npm scripts for pipeline

--- client/generate.sh
+++ client/generate.sh
@@ -3,7 +3,7 @@
 echo
 echo "Extracting translations from source code"
 echo "Compilation is done automatically by vite lingui plugin"
-npm run i18n:extract
+npm run translations:extract
 
 echo
 echo "Generating GraphQL code for client"
