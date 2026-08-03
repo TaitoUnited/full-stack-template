f62576622b35ac6f1b89c74af1f7f19c7a1497dc
Teemu Taskula <themetor@gmail.com>
2026-08-03 12:37:33 +0300

    chore(format): replace prettier with oxfmt

---
+++ server/oxfmt.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'oxfmt';
+
+export default defineConfig({
+  printWidth: 80,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  sortPackageJson: false,
+  ignorePatterns: ['./db/migrations/**/*', './test/graphql-test-env.d.ts'],
+});
